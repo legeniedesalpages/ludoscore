6c82830773255557b2c069744e8671ae8ded2268
Renaud <renaud_balu@hotmail.com>
2022-12-11 15:57:45 +0100

    deploiement

--- deployment/environnement.php
+++ deployment/environnement.php
@@ -1,19 +1,20 @@
 <?php
 
+	$data = json_decode(file_get_contents('php://input'), true);
 	$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-	$txt  = "APP_NAME=\"".htmlspecialchars($_POST["APP_NAME"])."\"\n";
-	$txt .= "APP_ENV=".htmlspecialchars($_POST["APP_ENV"])."\n";
-	$txt .= "APP_KEY=".htmlspecialchars($_POST["APP_KEY"])."\n";
-	$txt .= "APP_DEBUG=".htmlspecialchars($_POST["APP_DEBUG"])."\n";
-	$txt .= "APP_URL=".htmlspecialchars($_POST["APP_URL"])."\n";
-	$txt .= "LOG_CHANNEL=".htmlspecialchars($_POST["LOG_CHANNEL"])."\n";
-	$txt .= "LOG_DEPRECATIONS_CHANNEL=".htmlspecialchars($_POST["LOG_DEPRECATIONS_CHANNEL"])."\n";
-	$txt .= "LOG_LEVEL=".htmlspecialchars($_POST["LOG_LEVEL"])."\n";
-	$txt .= "DB_HOST=".htmlspecialchars($_POST["DB_HOST"])."\n";
-	$txt .= "DB_PORT=".htmlspecialchars($_POST["DB_PORT"])."\n";
-	$txt .= "DB_DATABASE=".htmlspecialchars($_POST["DB_DATABASE"])."\n";
-	$txt .= "DB_USERNAME=".htmlspecialchars($_POST["DB_USERNAME"])."\n";
-	$txt .= "DB_PASSWORD=".htmlspecialchars($_POST["DB_PASSWORD"])."\n";
-	$txt .= "DB_PREFIX=".htmlspecialchars($_POST["DB_PREFIX"])."\n";
+	$txt  = "APP_NAME=\"".htmlspecialchars($data["APP_NAME"])."\"\n";
+	$txt .= "APP_ENV=".htmlspecialchars($data["APP_ENV"])."\n";
+	$txt .= "APP_KEY=".htmlspecialchars($data["APP_KEY"])."\n";
+	$txt .= "APP_DEBUG=".htmlspecialchars($data["APP_DEBUG"])."\n";
+	$txt .= "APP_URL=".htmlspecialchars($data["APP_URL"])."\n";
+	$txt .= "LOG_CHANNEL=".htmlspecialchars($data["LOG_CHANNEL"])."\n";
+	$txt .= "LOG_DEPRECATIONS_CHANNEL=".htmlspecialchars($data["LOG_DEPRECATIONS_CHANNEL"])."\n";
+	$txt .= "LOG_LEVEL=".htmlspecialchars($data["LOG_LEVEL"])."\n";
+	$txt .= "DB_HOST=".htmlspecialchars($data["DB_HOST"])."\n";
+	$txt .= "DB_PORT=".htmlspecialchars($data["DB_PORT"])."\n";
+	$txt .= "DB_DATABASE=".htmlspecialchars($data["DB_DATABASE"])."\n";
+	$txt .= "DB_USERNAME=".htmlspecialchars($data["DB_USERNAME"])."\n";
+	$txt .= "DB_PASSWORD=".htmlspecialchars($data["DB_PASSWORD"])."\n";
+	$txt .= "DB_PREFIX=".htmlspecialchars($data["DB_PREFIX"])."\n";
 	fwrite($myfile, $txt);
 	fclose($myfile);
