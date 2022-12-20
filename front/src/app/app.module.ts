5692c3d7e4a61659dc286ca620db4427f141f75b
Renaud <renaud_balu@hotmail.com>
2022-12-20 14:55:56 +0100

    lint typescript

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -44,8 +44,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     MatMenuModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     })
   ],
