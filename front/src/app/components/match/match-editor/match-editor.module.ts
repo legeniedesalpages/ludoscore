758bf78ec284d027827ffd9c7299321633c1b9ad
Renaud <renaud_balu@hotmail.com>
2023-01-25 13:19:54 +0100

    filtre jeux

--- front/src/app/components/match/match-editor/match-editor.module.ts
+++ front/src/app/components/match/match-editor/match-editor.module.ts
@@ -14,11 +14,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuardService } from 'src/app/core/services/auth/auth-guard.service';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatchEditorComponent } from './match-editor.component';
 import { LoadingSpinnerModule } from '../../layout/spinner/loading-spinner.module';
 import { LayoutModule } from '../../layout/layout.module';
@@ -27,7 +24,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { A11yModule } from '@angular/cdk/a11y'
 import { FormsModule } from '@angular/forms';
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 const routes: Routes = [
   {
@@ -42,18 +39,16 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatMenuModule,
     LayoutModule,
-    MatProgressSpinnerModule,
     LoadingSpinnerModule,
     MatRippleModule,
     MatFormFieldModule,
     MatInputModule,
     A11yModule,
-    FormsModule
+    FormsModule,
+    MatCheckboxModule
   ]
 })
 export class MatchEditorModule { }
