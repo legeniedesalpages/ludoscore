c8db06ed03d04dd0f383f4cf90b5d30c332f1e1d
Renaud Balu <renaud_balu@hotmail.com>
2023-07-03 22:22:54 +0200

    page user create

---
+++ front/src/app/components/user/create/user-create.component.ts
@@ -0,0 +1,27 @@
+/**
+    * @description      :
+    * @author           : renau
+    * @group            :
+    * @created          : 19/12/2022 - 11:27:36
+    *
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 19/12/2022
+    * - Author          : renau
+    * - Modification    :
+**/
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  templateUrl: './user-create.component.html',
+  styleUrls: ['./user-create.component.css'],
+})
+export class UserCreateComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
