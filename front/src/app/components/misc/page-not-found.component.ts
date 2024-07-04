0168a2271d8de8925e3e94bf8ee0e9e2697061c0
Renaud <renaud_balu@hotmail.com>
2024-07-04 18:43:27 +0200

    mail de confirmation du compte

---
+++ front/src/app/components/misc/page-not-found.component.ts
@@ -0,0 +1,19 @@
+/**
+    * @description      : 
+    * @author           : renau
+    * @group            : 
+    * @created          : 04/07/2024 - 14:43:57
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 04/07/2024
+    * - Author          : renau
+    * - Modification    : 
+**/
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    template: `<div>No found</div>`
+})
+export class PagenotfoundComponent { }
