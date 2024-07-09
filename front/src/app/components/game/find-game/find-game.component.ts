d4063444bfef2729b3729e44812551a10fc2d53c
Renaud <renaud_balu@hotmail.com>
2024-07-09 10:02:53 +0200

    focus

--- front/src/app/components/game/find-game/find-game.component.ts
+++ front/src/app/components/game/find-game/find-game.component.ts
@@ -10,29 +10,11 @@
     * - Author          : renau
     * - Modification    : 
 **/
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FindGameService } from 'src/app/core/services/game/find-game.service';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { timer } from 'rxjs'
+import { FindGameService } from 'src/app/core/services/game/find-game.service'
 
-
-function focusAndOpenKeyboard(el: ElementRef<HTMLInputElement>, timeout: number) {
-  if(el) {
-    var tempElement = document.createElement('input');
-    tempElement.style.position = 'absolute';
-    tempElement.style.top = (el.nativeElement.offsetTop + 7) + 'px';
-    tempElement.style.left = el.nativeElement.offsetLeft + 'px';
-    tempElement.style.height = '0';
-    tempElement.style.opacity = '0';
-    document.body.appendChild(tempElement);
-    tempElement.focus();
-
-    setTimeout(function() {
-      el.nativeElement.focus();
-      el.nativeElement.click();
-      document.body.removeChild(tempElement);
-    }, timeout);
-  }
-}
 
 @Component({
   selector: 'find-game',
@@ -42,70 +24,72 @@ function focusAndOpenKeyboard(el: ElementRef<HTMLInputElement>, timeout: number)
 export class FindGameComponent implements OnInit, AfterViewInit {
 
   @ViewChild('recherche', {read: ElementRef, static: true})
-  firstElement?: ElementRef<HTMLInputElement>;  
+  firstElement?: ElementRef<HTMLInputElement>
   
-  public searching: boolean;
-  public searchString: string;
+  public searching: boolean
+  public searchString: string
 
   constructor(public findGameService: FindGameService, private router: Router, private route: ActivatedRoute) {
-    this.searching = false;
-    this.searchString = '';
+    this.searching = false
+    this.searchString = ''
   }
 
   ngAfterViewInit(): void {
-    this.firstElement?.nativeElement.click();
+    timer(500).subscribe(() => {
+      this.firstElement?.nativeElement.click()
+    })
   }
 
   ngOnInit() {
     this.route.queryParamMap.subscribe((params) => {  
-      let reset = params.get('no-reset');
+      let reset = params.get('no-reset')
       if (reset === 'true') {
         console.debug("No reset")
       } else {
-        this.findGameService.resetPreviousSearch();
+        this.findGameService.resetPreviousSearch()
       }
-    });
+    })
   }
 
   public search(event: any) {
 
     if (!event.target.value) {
-      console.debug('No search string');
-      return;
+      console.debug('No search string')
+      return
     }
 
     if  (event.target.value === this.searchString) {
-      console.debug('No new search, old search : ' + this.searchString);
-      return;
+      console.debug('No new search, old search : ' + this.searchString)
+      return
     }
 
-    this.searchString = event.target.value;
-    this.searching = true;
-    console.debug("Search: ", this.searchString);
+    this.searchString = event.target.value
+    this.searching = true
+    console.debug("Search: ", this.searchString)
 
     this.findGameService.search(this.searchString).subscribe({
 
       next: (res: any) => {
-        console.info("Result count: " + res.length);
-        this.searching = false;
+        console.info("Result count: " + res.length)
+        this.searching = false
       },
 
       error: () => {
-        this.searching = false;
+        this.searching = false
       }
-    });
+    })
   }
 
   public gameDetail(id: number, owned: boolean) {
-    console.debug("Go to edition page for id :" + id);
+    console.debug("Go to edition page for id :" + id)
     if (!owned) {
-      this.router.navigate(['/game-editor', 'bgg', id]);
+      this.router.navigate(['/game-editor', 'bgg', id])
     } else {
-      this.router.navigate(['/game-editor', 'owned', id]);
+      this.router.navigate(['/game-editor', 'owned', id])
     }
   }
 
   public returnToHome() { 
-    this.router.navigate(['/']);
+    this.router.navigate(['/'])
   }
 }
